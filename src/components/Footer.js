aa0d8c92bf81dad712bb32d03d1d28a474c5f0dc
xdd <xudd@deepblueai.com>
2021-05-24 23:04:46 +0800

    :lipstick: update content style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,16 @@
 const Footer = () => {
   return (
-    <footer className="py-4 text-center border divide-solid divide-gray-100 font-serif text-base">
-    © {new Date().getFullYear()}
-    {` `}
-    <a className="text-blue-600" href="https://github.com/aaronxdd">Dongdong Xu</a>, Hosted on
-    {` `}
-    <a className="text-blue-600" href="https://pages.github.com">GitHub Pages</a>
+    <footer className="py-4 text-center border-t divide-solid divide-gray-100 font-serif text-base">
+      © {new Date().getFullYear()}
+      {` `}
+      <a className="text-blue-600" href="https://github.com/aaronxdd">
+        Dongdong Xu
+      </a>
+      , Hosted on
+      {` `}
+      <a className="text-blue-600" href="https://pages.github.com">
+        GitHub Pages
+      </a>
     </footer>
   );
 };
