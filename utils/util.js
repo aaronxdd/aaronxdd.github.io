effde145ce57492cf12565f680df98dbbbb4be53
xdd <xudd@deepblueai.com>
2021-06-15 16:14:24 +0800

    :bug: no-cors

--- utils/util.js
+++ utils/util.js
@@ -1,22 +1,30 @@
 export const getDay = () => {
-  const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
   return days[new Date().getDay()];
-}
+};
 
-export const request = async(url) => {
-  const res = await fetch(url);
+export const request = async (url) => {
+  const res = await fetch(url, { mode: "no-cors" });
   const data = await res.json();
   return data;
-}
+};
 
 export const getCity = () => {
   if ("geolocation" in navigator) {
     /* 地理位置服务可用 */
-      console.log('----geolocation----');
-    navigator.geolocation.getCurrentPosition(function(position) {
-      console.log('----position----', position);
+    console.log("----geolocation----");
+    navigator.geolocation.getCurrentPosition(function (position) {
+      console.log("----position----", position);
     });
   } else {
     /* 地理位置服务不可用 */
   }
-}
+};
