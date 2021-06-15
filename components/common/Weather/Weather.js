3fd39b3ad23ec27cc98d7355b9710847a74ebe82
xdd <xudd@deepblueai.com>
2021-06-15 16:06:14 +0800

    :bug: 修复请求地址

--- components/common/Weather/Weather.js
+++ components/common/Weather/Weather.js
@@ -7,11 +7,11 @@ const Weather = () => {
 
   useEffect(async () => {
     const domain =
-      process.env.NODE_ENV === "development" ? "/" : "http://apis.juhe.cn/";
+      process.env.NODE_ENV === "development" ? "/api" : "https://apis.juhe.cn";
     const data = await request(
-      `${domain}api/simpleWeather/query?city=%E4%B8%8A%E6%B5%B7&key=f452564f45875556bbdc49d9217b7952`
+      `${domain}/simpleWeather/query?city=%E4%B8%8A%E6%B5%B7&key=f452564f45875556bbdc49d9217b7952`
     );
-    // getCity();
+    getCity();
     const { error_code, result } = data;
     if (error_code === 0) {
       const {
